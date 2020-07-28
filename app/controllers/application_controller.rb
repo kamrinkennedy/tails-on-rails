20a2a1cb6782a1b8cab42177bb667b2df7e02f93
Kamrin <kamrinkennedy@gmail.com>
2020-07-28 12:06:04 -0500

    controller routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+
+    def logged_in?
+        !!current_user
+    end 
+
+    def current_user
+        User.find_by_id(session[:user_id])
+    end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   resources :recipes
-  resources :sessions
-  resources :users
+  resources :sessions, only: [:new, :create]
+  resources :users, only: [:new, :create, :show, :index]
+
+  root "welcome#home"
+  get '/signup', to: "user#new"
+  get '/login', to: "session#new"
+  delete '/logout', to: "sessions#delete"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
